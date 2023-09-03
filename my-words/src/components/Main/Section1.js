c04bf5c89f94ea943309ef2e75bd29d4fcc79ca0
morbdeath <nahuel_barbosa1990@hotmail.com.ar>
2023-09-03 20:45:54 -0300

    Se armo el listado y el recorrido, se corrobora los aciertos y en el ultimo de la lista aparece el boton resultados

--- my-words/src/components/Main/Section1.js
+++ my-words/src/components/Main/Section1.js
@@ -5,40 +5,104 @@ export default function Section1() {
 
     const [inputValue, setInputValue] = useState('');
     const [labelValue, setLabelValue] = useState('');
-    const [listaPalabras, setListaPalabras] = useState([]);
-
-    const palabrasIngles = ["red","yellow","blue"];
+    const [listaPalabras, setListaPalabras] = useState([
+        {
+            id: 1,
+            palIngles: "red",
+            palEspañol: "rojo",
+            estado: true,
+        },
+        {
+            id: 2,
+            palIngles: "house",
+            palEspañol: "casa",
+            estado: true,
+        },
+        {
+            id: 3,
+            palIngles: "tree",
+            palEspañol: "arbol",
+            estado: true,
+        }
+    ]);
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
     };
 
+    const [indicePalabra, setIndicePalabra] = useState(0);
+    const [mostrarBotonPresionar, setMostrarBotonPresionar] = useState(true);
+    //let indicePalabraTemp = indicePalabra;
+    let currentIndex = indicePalabra;
     const handleButtonClick = () => {
-        setLabelValue(inputValue);
-        setListaPalabras([...listaPalabras, inputValue])
+
+
+        //listaPalabras.forEach((dato) => {
+        /*if (inputValue === listaPalabras.palEspañol) {
+            console.log("funciono");
+            console.log(dato.id - 1);
+        }*/
+        for (let i = 0; i < listaPalabras.length; i++) {
+            if (inputValue === listaPalabras[i].palEspañol) {
+                console.log("funciono");
+            }
+        }
+        // });
+
+        console.log(currentIndex);
+        console.log(indicePalabra);
+        if (currentIndex < listaPalabras.length) {
+            setIndicePalabra(currentIndex + 1);
+        } else if (currentIndex === listaPalabras.length1) {
+            console.log("se acabo la lista");
+            setInputValue("");
+            console.log(listaPalabras.length);
+            setMostrarBotonPresionar(false);
+        }
         setInputValue("");
     };
 
+    const botonResultados = (
+        <button> Boton resultados</button>
+    );
+
+
     let temporaryInput = ''; // Variable para almacenar temporalmente el valor del campo
+
+    const p = listaPalabras.map((data) =>
+        <li key={data.id}>
+            {data.id}
+            {data.palIngles}
+        </li>
+    );
 
     return (
 
         <div className="content-section1">
             <div className="content-palabra">
-                <label> Dato ingresado: {listaPalabras}</label>
+                <ul>
+                    {p}
+                </ul>
+                <br /><br />
+
+                <br /><br />
+
+
+                <label> palabra en inglés: {indicePalabra < listaPalabras.length ? listaPalabras[indicePalabra].palIngles : 'Fin de la lista'} </label>
                 <input
                     type="text"
+                    placeholder='Ingresa la palabra en español'
                     value={inputValue}
                     onChange={handleInputChange}
                 />
-                <Button onPresionar={handleButtonClick} />
-
+                <Button onPresionar={handleButtonClick} sacarBoton={setMostrarBotonPresionar} />
+                <Button mostrarBotonResultados={indicePalabra === listaPalabras.length} />
             </div>
         </div>
     );
 }
 
-function Button({ onPresionar }) {
+function Button({ onPresionar, mostrarBotonResultados, sacarBoton }) {
 
     const cargarPalabra = () => {
         const nuevoDato = "Palabra";
@@ -48,9 +112,14 @@ function Button({ onPresionar }) {
 
     return (
         <>
-            <button onClick={cargarPalabra}>
-                Presionar
-            </button>
+            {sacarBoton ? null : (
+                <Button sacarBoton={sacarBoton} />
+            )}
+        
+
+            {mostrarBotonResultados && (
+                <button>Resultados</button>
+            )}
         </>
     );
 }
